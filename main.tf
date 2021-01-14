c9480c4580e3b0bb1d21eda31746fadc1db6b0c2
Allan Roscoche <allan.roscoche@savecash.com.br>
2021-01-14 18:16:40 -0300

    feat: add update endpoint

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-
-  # Make it faster by skipping something
-  #skip_get_ec2_platforms      = true
-  #skip_metadata_api_check     = true
-  #skip_region_validation      = true
-  #skip_credentials_validation = true
-
-  # skip_requesting_account_id should be disabled to generate valid ARN in apigatewayv2_api_execution_arn
-  #skip_requesting_account_id = false
 }
 
 resource "aws_cloudwatch_log_group" "logs" {
@@ -33,21 +24,35 @@ module "api_gateway" {
   }
 
   create_api_domain_name = false
-  #domain_name                 = "terraform-aws-modules.modules.tf"
-  #domain_name_certificate_arn = module.acm.this_acm_certificate_arn
 
   default_stage_access_log_destination_arn = aws_cloudwatch_log_group.logs.arn
-  default_stage_access_log_format          = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
+  default_stage_access_log_format = "$context.identity.sourceIp - - [$context.requestTime] \"$context.httpMethod $context.routeKey $context.protocol\" $context.status $context.responseLength $context.requestId $context.integrationErrorMessage"
 
   integrations = {
-    "ANY /" = {
-      lambda_arn             = module.lambda_function.this_lambda_function_arn
+    "GET /{id}" = {
+      lambda_arn             = module.lambda_function_consulta_funcionario.this_lambda_function_arn
+      payload_format_version = "2.0"
+      timeout_milliseconds   = 12000
+    }
+    "DELETE /{id}" = {
+      lambda_arn             = module.lambda_function_apaga_funcionario.this_lambda_function_arn
+      payload_format_version = "2.0"
+      timeout_milliseconds   = 12000
+    }
+    "PUT /{id}" = {
+      lambda_arn             = module.lambda_function_atualiza_funcionario.this_lambda_function_arn
+      payload_format_version = "2.0"
+      timeout_milliseconds   = 12000
+    }
+ 
+    "POST /" = {
+      lambda_arn             = module.lambda_function_cria_funcionario.this_lambda_function_arn
       payload_format_version = "2.0"
       timeout_milliseconds   = 12000
     }
 
     "$default" = {
-      lambda_arn = module.lambda_function.this_lambda_function_arn
+      lambda_arn = module.lambda_function_consulta_funcionario.this_lambda_function_arn
     }
 
   }
@@ -58,15 +63,15 @@ module "api_gateway" {
 #############################################
 
 
-module "lambda_function" {
+module "lambda_function_cria_funcionario" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "~> 1.0"
 
-  function_name = "ton-test-lambda"
-  description   = "My awesome lambda function"
-  handler       = "hello.handler"
+  function_name = "ton-cria-funcionario"
+  description   = "Cria Funcionario"
+  handler       = "create.handler"
   runtime       = "nodejs10.x"
-  source_path   = "src/hello.js"
+  source_path   = "src"
 
   publish = true
 
@@ -80,6 +85,75 @@ module "lambda_function" {
     }
   }
 }
+module "lambda_function_consulta_funcionario" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "~> 1.0"
+
+  function_name = "ton-consulta-funcionario"
+  description   = "Consulta Funcionario"
+  handler       = "get.handler"
+  runtime       = "nodejs10.x"
+  source_path   = "src"
+
+  publish = true
+
+  attach_policy_json = true
+  policy_json = file("policy.json")
+
+  allowed_triggers = {
+    AllowExecutionFromAPIGateway = {
+      service = "apigateway"
+      arn     = module.api_gateway.this_apigatewayv2_api_execution_arn
+    }
+  }
+}
+module "lambda_function_apaga_funcionario" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "~> 1.0"
+
+  function_name = "ton-apaga-funcionario"
+  description   = "Consulta Funcionario"
+  handler       = "delete.handler"
+  runtime       = "nodejs10.x"
+  source_path   = "src"
+
+  publish = true
+
+  attach_policy_json = true
+  policy_json = file("policy.json")
+
+  allowed_triggers = {
+    AllowExecutionFromAPIGateway = {
+      service = "apigateway"
+      arn     = module.api_gateway.this_apigatewayv2_api_execution_arn
+    }
+  }
+}
+module "lambda_function_atualiza_funcionario" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "~> 1.0"
+
+  function_name = "ton-atualiza-funcionario"
+  description   = "Consulta Funcionario"
+  handler       = "update.handler"
+  runtime       = "nodejs10.x"
+  source_path   = "src"
+
+  publish = true
+
+  attach_policy_json = true
+  policy_json = file("policy.json")
+
+  allowed_triggers = {
+    AllowExecutionFromAPIGateway = {
+      service = "apigateway"
+      arn     = module.api_gateway.this_apigatewayv2_api_execution_arn
+    }
+  }
+}
+
+
+
 
 #######################################
 # DynamoDB 
--- src/update.js
+++ src/update.js
@@ -1,10 +1,18 @@
 const Funcionario = require('./funcionario');
 
+function parseBody(query) {
+  let buff = Buffer.from(query, "base64");
+  let eventBodyStr = buff.toString('UTF-8');
+  let eventBody = {};
+  eventBodyStr.split('&').forEach( i => { let atr = i.split('='); eventBody[atr[0]]=atr[1] });
+  return eventBody;
+}
+
 exports.handler = async (event) => {
   const id = event.pathParameters.id
-  const { nome, idade, cargo } = event;
   try {
     const funcionario = await Funcionario.find(id); 
+    const { nome, idade, cargo } = parseBody(event.body);
     funcionario.nome = nome;
     funcionario.idade = idade;
     funcionario.cargo = cargo;
